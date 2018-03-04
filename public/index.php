a2d42ed12c687e532f33d8cb15cac19184e13e98
Levente Löki <leventeloki@gmail.com>
2018-03-04 14:07:49 +0100

    Installed auryn

--- public/index.php
+++ public/index.php
@@ -1,3 +1,17 @@
 <?php
 
-echo 1;
+namespace App;
+
+use Auryn\Injector;
+use Lencse\WorkCalendar\Calendar\Repository\DayTypeRepository;
+use Lencse\WorkCalendar\Hu\Repository\HuDayTypeRepository;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$injector = new Injector();
+
+$injector->alias(DayTypeRepository::class, HuDayTypeRepository::class);
+
+$repo = $injector->make(DayTypeRepository::class);
+
+var_dump($repo->getAll());
